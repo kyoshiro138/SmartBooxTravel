72e04b1c3ed25d79443d3b435f503e2b2c394303
tuu.phung <tuu.phung@sutrixmedia.com>
2015-09-11 11:48:10 +0700

    Add map fragment for place and location

---
+++ app/src/main/java/com/smartboox/travel/app/placemap/PlaceMapFragment.java
@@ -0,0 +1,22 @@
+package com.smartboox.travel.app.placemap;
+
+import android.view.View;
+
+import com.smartboox.travel.appimplementation.fragment.AppFragment;
+
+public class PlaceMapFragment extends AppFragment {
+    @Override
+    protected int getFragmentLayoutResource() {
+        return 0;
+    }
+
+    @Override
+    protected void bindView(View rootView) {
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+}
